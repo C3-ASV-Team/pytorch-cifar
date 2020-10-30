906d3d52b2e17731284378746e23a4425a73065d
zhengzangw <zhengzangw@163.com>
2020-10-30 13:58:43 +0800

    add --gpus=2 to correctly request gpu usage
    without --gpus parameters, sbatch cannot correctly apply gpus.
    also add some comments for usage

--- train_cgpu.sh
+++ train_cgpu.sh
@@ -2,12 +2,17 @@
 #SBATCH --qos=regular
 #SBATCH --constraint=gpu
 #SBATCH --nodes=1
+#SBATCH --gpus=2
 #SBATCH --time=00:05:00
-#SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=10
 #SBATCH --ntasks=1
 
+#SBATCH --mail-type=ALL
+##SBATCH --output=%j-%x.%u.out
+##SBATCH --job-name=cifar
+
 module load pytorch/v1.5.0-gpu
+## conda activate env_name
 
 srun python main.py
 
